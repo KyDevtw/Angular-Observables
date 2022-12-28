0bd95f731663e1bee55c668bdf45ff836f0810e9
Kurt <k3030789@gmail.com>
2022-12-28 23:14:51 +0800

    [observable] create interval from rxjs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  private firstObsSubscription: Subscription;
   constructor() { }
 
   ngOnInit() {
+    // 透過 firstObsSubscription 去訂閱與銷毀 interval
+    this.firstObsSubscription = interval(1000).subscribe(
+      count => {
+        console.log(count)
+      }
+    )
   }
 
+  ngOnDestroy(): void {
+    // unsubscribe() 銷毀 interval
+    this.firstObsSubscription.unsubscribe();
+  }
 }
